1e8df1f4f367513b0c366d9e4e50406f20e44909
Paula McKinley <pmknyc@optimum.net>
2018-08-14 20:24:10 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
+require 'pry'
 #  method called #fizzbuzz that accepts one argument.
-
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
@@ -11,8 +11,10 @@ def fizzbuzz(int)
   elsif int % 15 == 0
     puts "FizzBuzz" 
   #else return nil
+  binding.pry
   end
 end
+fizzbuzz(3)
 
 # when call fizzbuzz method & pass number divisible by 3, it should  return the string "Fizz"
 # create fizz
